63abc22bc8b6e9289b476e6d86e160a7658fd22c
Erik Pearson <emp823@gmail.com>
2014-02-16 18:44:56 -0500

    First commit for Donator app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Donator::Application.config.secret_key_base = '3c3fac97804da3e42f6dc741cd3165bffd0de7b4383c1b2f3309fa5fb27210339be83c2a21b4f3964b72d27ca0eb50b100596457a16a84c0792ae7429f31e38e'

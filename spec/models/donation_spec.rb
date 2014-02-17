f77972dff20882fc1e26db868a744364d09a3e9d
Erik Pearson <emp823@gmail.com>
2014-02-17 03:36:53 -0500

    Check attributes of donations

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -4,4 +4,35 @@ describe Donation do
   let(:item_donation) { FactoryGirl.create(:item_donation) }
   let(:voucher_donation) { FactoryGirl.create(:voucher_donation) }
   let(:experience_donation) { FactoryGirl.create(:experience_donation) }
+
+  subject { item_donation }
+
+  describe "accessible attributes" do
+    it { should respond_to(:user) }
+    it { should respond_to(:title) }
+    it { should respond_to(:description) }
+    it { should respond_to(:donatable_type) }
+    it { should respond_to(:donatable_id) }
+  end
+
+  subject { voucher_donation }
+
+  describe "accessible attributes" do
+    it { should respond_to(:user) }
+    it { should respond_to(:title) }
+    it { should respond_to(:description) }
+    it { should respond_to(:donatable_type) }
+    it { should respond_to(:donatable_id) }
+  end
+
+  subject { experience_donation }
+
+  describe "accessible attributes" do
+    it { should respond_to(:user) }
+    it { should respond_to(:title) }
+    it { should respond_to(:description) }
+    it { should respond_to(:donatable_type) }
+    it { should respond_to(:donatable_id) }
+  end
+
 end

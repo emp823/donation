0fcfe2db0f2bf650dd2745b1d468391ed19f5cd2
Erik Pearson <emp823@gmail.com>
2014-02-16 19:16:30 -0500

    Change token file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,17 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Donator::Application.config.secret_key_base = '3c3fac97804da3e42f6dc741cd3165bffd0de7b4383c1b2f3309fa5fb27210339be83c2a21b4f3964b72d27ca0eb50b100596457a16a84c0792ae7429f31e38e'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token

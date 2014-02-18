eb4c5695eec411eb22bfe7a600c83e90d68eea49
Erik Pearson <emp823@gmail.com>
2014-02-18 00:53:17 -0500

    Adds beginning of donatino form

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,42 @@
+class DonationsController < ApplicationController
+  def new
+    @donation = Donation.new()
+  end
+
+  def create
+    @donation = Donation.new(donation_params)
+    if @donation.save
+      redirect_to @donation, notice: "Successfully created donation!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @donation = Donation.find(params[:id])
+    if @donation.update_attributes(donation_params)
+      redirect_to @donation, notice: "Updated donation!"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @donation = donation.find(params[:id])
+  end
+
+  def destroy
+    @donation = donation.find(params[:id])
+    @donation.destroy
+    redirect_to donations_url, notice: "Destroyed donation"
+  end
+
+  def show
+  end
+
+  private
+
+    def donation_params
+      params.require(:donation).permit(:id, :title, :description, :user_id, :donatable_id, :donatable_type)
+    end
+end
--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,3 +1,4 @@
 class Voucher < ActiveRecord::Base
   has_one :donation, :as => :donatable
+  validates_presence_of :expiration_date
 end

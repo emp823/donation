0f364039c257e3639014855c7993c16e1052309d
Erik Pearson <emp823@gmail.com>
2014-02-19 01:15:05 -0500

    Adds lat lon validation

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,8 @@
 class Experience < ActiveRecord::Base
   has_one :donation, :as => :donatable
   validates_presence_of :latitude, :longitude, :contact_name
+  validates :latitude , numericality: { greater_than:  -90, less_than:  90 }
+  validates :longitude, numericality: { greater_than: -180, less_than: 180 }
 
   accepts_nested_attributes_for :donation
 end
